c360cf59122612394ddc52b6b5d5d21031e46de8
Don Cadavona <dcadavona@gmail.com>
2024-07-19 06:04:55 +0000

    Throw an informative error when there is no name set for the customer

--- src/Concerns/ManagesCustomer.php
+++ src/Concerns/ManagesCustomer.php
@@ -45,6 +45,10 @@ trait ManagesCustomer
             $response = Cashier::api('POST', 'customers', $options)['data'];
         }
 
+        if (is_null($response['name'])) {
+            throw new LogicException("The Paddle customer [{$response['id']}] has no name. You may need to set the name of the customer in the Paddle dasbhboard.");
+        }
+
         if (Cashier::$customerModel::where('paddle_id', $response['id'])->exists()) {
             throw new LogicException("The Paddle customer [{$response['id']}] already exists in the database.");
         }
